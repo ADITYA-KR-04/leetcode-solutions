c1810fd365199fe24fbd32753032527cef1fa247
ADITYA-KR-04 <153849992+ADITYA-KR-04@users.noreply.github.com>
2024-03-25 16:01:30 +0530

    Time: 115 ms (5.31%) | Memory: 12.5 MB (98.71%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        i=0
+        while(i<(len(asteroids)-1)):
+            if (asteroids[i]*asteroids[i+1])>0 or (asteroids[i]<0 and asteroids[i+1]>0):
+                i+=1
+            else:
+                if asteroids[i]==(-1)*asteroids[i+1]:
+                    asteroids.pop(i+1)
+                    asteroids.pop(i)
+                elif abs(asteroids[i])>abs(asteroids[i+1]):
+                    asteroids.pop(i+1)
+                else:
+                    asteroids.pop(i)
+                if i>0:
+                    i-=1
+                else:
+                    continue
+        return asteroids
