6d4746bfd5f8160fbe3acc88c37068ba9e99d1d7
ADITYA-KR-04 <153849992+ADITYA-KR-04@users.noreply.github.com>
2024-03-25 16:16:37 +0530

    Time: 113 ms (5.40%) | Memory: 12.6 MB (92.12%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         i=0
         while(i<(len(asteroids)-1)):
-            if (asteroids[i]*asteroids[i+1])>0 or (asteroids[i]<0 and asteroids[i+1]>0):
+            if (asteroids[i]>0 and asteroids[i+1]>0) or (asteroids[i]<0 and asteroids[i+1]<0) or (asteroids[i]<0 and asteroids[i+1]>0):
                 i+=1
             else:
                 if asteroids[i]==(-1)*asteroids[i+1]:
