9fbebee001e4bb43ffd3ab520da396f99879a776
ADITYA-KR-04 <153849992+ADITYA-KR-04@users.noreply.github.com>
2025-03-13 16:40:44 +0530

    Time: 537 ms (64.93%) | Memory: 33.8 MB (55.70%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,25 +1,17 @@
 class Solution(object):
     def longestCommonSubsequence(self, text1, text2):
-        """
-        :type text1: str
-        :type text2: str
-        :rtype: int
-        """
-        dp={}
-        m=len(text1)
-        n=len(text2)
-        def solve(text1,text2,m,n):
-            if m==0 or n==0:
-                return 0
-            elif (m,n) in dp:
-                return dp[(m,n)]
-            else:
-                if text1[m-1]==text2[n-1]:
-                    c=1+solve(text1,text2,m-1,n-1)
-                else:
-                    c1=solve(text1,text2,m-1,n)
-                    c2=solve(text1,text2,m,n-1)
-                    c=max(c1,c2)
-                dp[(m,n)]=c
-                return c
-        return solve(text1,text2,m,n)
+        
+        n, m = len(text1), len(text2)
+
+        # Create a DP table with (m+1) x (n+1) dimensions
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        # Fill the DP table
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[j - 1] == text2[i - 1]:  # Match case
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:  # No match, take max of previous results
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+        return dp[m][n]
